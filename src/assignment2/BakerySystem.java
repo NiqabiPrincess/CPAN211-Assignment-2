d41a9f4bf1a8d58f4d1574087ec9ba24561b0a9b
Zainab Aamir <2002zainabaamir@gmail.com>
2025-10-17 22:29:23 -0400

    Created labels, test fields, combo box, radio buttons and buttons

---
+++ src/assignment2/BakerySystem.java
@@ -0,0 +1,53 @@
+package assignment2;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+
+public class BakerySystem extends Application{
+	
+
+	@Override
+	public void start(Stage primaryStage){
+		primaryStage.setTitle("Bakery System");
+		
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		
+		
+		Label nameLabel = new Label("Customer Name: ");
+		TextField nameField = new TextField();
+		
+		Label phoneNumberLabel = new Label("Customer Phone Number: ");
+		TextField phoneNumberField = new TextField();
+		
+		Label cakeType = new Label("Cake Type Selected: ");
+		ComboBox<String> cakeTypeComboBox = new ComboBox<>();
+		cakeTypeComboBox.getItems().addAll("Apple", "Carrot", "Cheesecake", "Chocolate", "Coffee", "Opera", "Tiramisu");
+		
+		Label sizeLabel = new Label("Cake Size: ");
+		RadioButton smallRadioButton = new RadioButton("Small");
+		RadioButton mediumRadioButton = new RadioButton("Medium");
+		RadioButton largeRadioButton = new RadioButton("Large");
+		
+		Button saveButton = new Button("Save");
+		
+		Button quitButton = new Button("Quit");
+	
+		
+		
+	}
+	
+	
+
+}
--- src/assignment2/Main.java
+++ src/assignment2/Main.java
@@ -1,5 +1,7 @@
 package assignment2;
 
 public class Main {
-
+    public static void main(String[] args) {
+        
+    }
 }
